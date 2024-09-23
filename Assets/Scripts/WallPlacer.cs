3f092ea54794f30323c9690826f183bb76a67b88
adlai <adlaibridsonboyczuk@gmail.com>
2024-09-23 14:58:09 -0400

    Added multiple types of building material + ability to add more in future

--- Assets/Scripts/WallPlacer.cs
+++ Assets/Scripts/WallPlacer.cs
@@ -4,46 +4,54 @@ using UnityEngine;
 
 public class WallPlacer : MonoBehaviour
 {
-    public GameObject wallPrefab;
-    public GameObject wallPreviewPrefab;
-
+    public GameObject[] buildablePrefabs;
+    public GameObject currentPrefab;
     private GameObject previewInstance;
     private bool isPlacing = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (isPlacing)
+        if (isPlacing && currentPrefab != null)
         {
             UpdatePreviewPosition();
 
             // Place the wall on left mouse click
             if (Input.GetMouseButtonDown(0))
             {
-                PlaceWall();
+                PlaceObject();
             }
         }
     }
 
-    public void TogglePlacingWall()
-    {
-        isPlacing = !isPlacing;
+    public void StartPlacingObject(int prefabIndex) {
+        if (prefabIndex >= 0 && prefabIndex < buildablePrefabs.Length) {
+            currentPrefab = buildablePrefabs[prefabIndex];
+            isPlacing = true;
 
-        if (isPlacing)
-        {
-            // Create the preview instance when starting placement
-            previewInstance = Instantiate(wallPreviewPrefab);
-        }
-        else
-        {
-            // Destroy the preview instance when stopping placement
-            if (previewInstance != null)
-            {
+            if (previewInstance != null) {
                 Destroy(previewInstance);
             }
-        }
 
-        Debug.Log("Wall placement " + (isPlacing ? "started" : "stopped"));
+            previewInstance = Instantiate(currentPrefab);
+            var spriteRenderer = previewInstance.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null) {
+                Color semiTransparent = spriteRenderer.color;
+                semiTransparent.a = 0.5f;
+                spriteRenderer.color = semiTransparent;
+            }
+        }
+    }
+
+    public void StopPlacingObject()
+    {
+        isPlacing = false;
+        currentPrefab = null;
+
+        if (previewInstance != null)
+        {
+            Destroy(previewInstance);
+        }
     }
 
     private void UpdatePreviewPosition()
@@ -53,23 +61,23 @@ public class WallPlacer : MonoBehaviour
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2Int gridPosition = new Vector2Int(Mathf.FloorToInt(mousePosition.x), Mathf.FloorToInt(mousePosition.y));
 
-        // Update the position of the preview wall
+        // Update the position of the preview object
         previewInstance.transform.position = new Vector3(gridPosition.x + 0.5f, gridPosition.y + 0.5f, 0);
     }
 
-    private void PlaceWall()
+    private void PlaceObject()
     {
         Vector2Int gridPosition = new Vector2Int(Mathf.FloorToInt(previewInstance.transform.position.x), Mathf.FloorToInt(previewInstance.transform.position.y));
 
         if (IsValidPlacement(gridPosition))
         {
-            Instantiate(wallPrefab, previewInstance.transform.position, Quaternion.identity);
+            Instantiate(currentPrefab, previewInstance.transform.position, Quaternion.identity);
         }
     }
 
     private bool IsValidPlacement(Vector2Int position)
     {
-        // Add logic to check if a wall can be placed here (e.g., no other wall or object present)
+        // Add logic to check if the object can be placed here
         return true; // Adjust this as needed
     }
 }
