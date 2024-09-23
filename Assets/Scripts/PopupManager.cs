ce2d773acafdeeafdc7ab4c63e290d96c81cc961
adlai <adlaibridsonboyczuk@gmail.com>
2024-09-23 16:26:53 -0400

    Added basic recruitment + laptop interaction

--- Assets/Scripts/PopupManager.cs
+++ Assets/Scripts/PopupManager.cs
@@ -1,9 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PopupManager : MonoBehaviour
 {
+    public RecruitableNPC[] recruitableNPCs;
+    public Transform npcListContainer;
+    public GameObject npcButtonPrefab;
+    public TextMeshProUGUI npcDescriptionText;
+    public Button confirmButton;
+    public Image npcMugshotImage;
+
+    private RecruitableNPC selectedNPC;
+
+    void Start() {
+        PopulateNPCList();
+        confirmButton.onClick.AddListener(RecruitSelectedNPC);
+        confirmButton.interactable = false;
+    }
+
+    void PopulateNPCList() {
+        foreach (RecruitableNPC npc in recruitableNPCs) {
+            GameObject npcButton = Instantiate(npcButtonPrefab, npcListContainer);
+            TextMeshProUGUI buttonText = npcButton.GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = npc.npcName;
+
+            Button buttonComponent = npcButton.GetComponent<Button>();
+            buttonComponent.onClick.AddListener(() => SelectNPC(npc));
+        }
+    }
+
+    void SelectNPC(RecruitableNPC npc) {
+        selectedNPC = npc;
+        npcDescriptionText.text = npc.description;
+        npcMugshotImage.sprite = npc.npcMugshot;
+        confirmButton.interactable = true;
+    }
+
+    void RecruitSelectedNPC(){
+        if (selectedNPC != null) {
+            Instantiate(selectedNPC.npcPrefab, Vector3.zero, Quaternion.identity);
+            Debug.Log(selectedNPC.npcName + " has been recruited");
+            ClosePopup();
+        }
+    }
+
     public void ClosePopup() {
         gameObject.SetActive(false);
     }
