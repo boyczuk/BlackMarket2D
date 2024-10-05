58fff0fac3f8c54fd9ca75f38f36d829b818a20a
adlai <adlaibridsonboyczuk@gmail.com>
2024-10-05 01:26:44 -0400

    Fixed UI manager update + modified scripts to properly close building interaction

--- Assets/Scripts/WallPlacer.cs
+++ Assets/Scripts/WallPlacer.cs
@@ -9,14 +9,12 @@ public class WallPlacer : MonoBehaviour
     private GameObject previewInstance;
     private bool isPlacing = false;
 
-    // Update is called once per frame
     void Update()
     {
         if (isPlacing && currentPrefab != null)
         {
             UpdatePreviewPosition();
 
-            // Place the wall on left mouse click
             if (Input.GetMouseButtonDown(0))
             {
                 PlaceObject();
@@ -24,18 +22,28 @@ public class WallPlacer : MonoBehaviour
         }
     }
 
-    public void StartPlacingObject(int prefabIndex) {
-        if (prefabIndex >= 0 && prefabIndex < buildablePrefabs.Length) {
+    public void StartPlacingObject(int prefabIndex)
+    {
+        if (isPlacing && currentPrefab == buildablePrefabs[prefabIndex])
+        {
+            StopPlacingObject();
+            return;
+        }
+
+        if (prefabIndex >= 0 && prefabIndex < buildablePrefabs.Length)
+        {
             currentPrefab = buildablePrefabs[prefabIndex];
             isPlacing = true;
 
-            if (previewInstance != null) {
+            if (previewInstance != null)
+            {
                 Destroy(previewInstance);
             }
 
             previewInstance = Instantiate(currentPrefab);
             var spriteRenderer = previewInstance.GetComponent<SpriteRenderer>();
-            if (spriteRenderer != null) {
+            if (spriteRenderer != null)
+            {
                 Color semiTransparent = spriteRenderer.color;
                 semiTransparent.a = 0.5f;
                 spriteRenderer.color = semiTransparent;
@@ -56,18 +64,28 @@ public class WallPlacer : MonoBehaviour
 
     private void UpdatePreviewPosition()
     {
-        if (previewInstance == null) return;
+        if (previewInstance == null)
+            return;
 
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2Int gridPosition = new Vector2Int(Mathf.FloorToInt(mousePosition.x), Mathf.FloorToInt(mousePosition.y));
+        Vector2Int gridPosition = new Vector2Int(
+            Mathf.FloorToInt(mousePosition.x),
+            Mathf.FloorToInt(mousePosition.y)
+        );
 
-        // Update the position of the preview object
-        previewInstance.transform.position = new Vector3(gridPosition.x + 0.5f, gridPosition.y + 0.5f, 0);
+        previewInstance.transform.position = new Vector3(
+            gridPosition.x + 0.5f,
+            gridPosition.y + 0.5f,
+            0
+        );
     }
 
     private void PlaceObject()
     {
-        Vector2Int gridPosition = new Vector2Int(Mathf.FloorToInt(previewInstance.transform.position.x), Mathf.FloorToInt(previewInstance.transform.position.y));
+        Vector2Int gridPosition = new Vector2Int(
+            Mathf.FloorToInt(previewInstance.transform.position.x),
+            Mathf.FloorToInt(previewInstance.transform.position.y)
+        );
 
         if (IsValidPlacement(gridPosition))
         {
@@ -77,8 +95,6 @@ public class WallPlacer : MonoBehaviour
 
     private bool IsValidPlacement(Vector2Int position)
     {
-        // Add logic to check if the object can be placed here
-        return true; // Adjust this as needed
+        return true;
     }
 }
-
